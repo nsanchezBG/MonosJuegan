3c5af90339d7fca7bdd12b0272fa4f633ce5bcce
nsanchezBG <nicolas@brandgeeks.pe>
2025-08-16 21:05:48 -0500

    Create serviceworker.js

---
+++ serviceworker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'pareja-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './questions_pareja.json',
+  
+  // Lista de todos los assets gráficos
+  './apple-touch-icon.png',
+  './favicon.ico',
+  './android-chrome-192x192.png',
+  './android-chrome-512x512.png',
+  './favicon-16x16.png',
+  './favicon-32x32.png',
+  './LogoPareja.png',
+  './IconoEspejo.png',
+  './IconoEquipo.png',
+  './IconoFuego.png',
+  './IconoEspejoInner.png',
+  './IconoEquipoInner.png',
+  './IconoFuegoInner.png',
+  './IconoBack.png',
+  
+  'https://fonts.googleapis.com/css2?family=Shrikhand&display=swap'
+];
+
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME];
+  e.waitUntil(caches.keys().then(names => Promise.all(names.map(name => { if (cacheWhitelist.indexOf(name) === -1) { return caches.delete(name); } }))));
+});
