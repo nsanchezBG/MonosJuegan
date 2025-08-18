f9e3050e12fdad0c9b9791824ed8e20a0a3983c8
nsanchezBG <nicolas@brandgeeks.pe>
2025-08-18 15:58:12 -0500

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,33 +1,35 @@
-const CACHE_NAME = 'pareja-cache-v2'; // Subimos la versión
+const CACHE_NAME = 'pareja-cache-v3'; // Subimos la versión
 const urlsToCache = [
-'./',
-'./index.html',
-'./style.css',
-'./script.js',
-'./manifest.json',
-'./questions_pareja.json',
-// Lista de todos los assets gráficos
-'./apple-touch-icon.png',
-'./favicon.ico',
-'./android-chrome-192x192.png',
-'./android-chrome-512x52.png',
-'./favicon-16x16.png',
-'./favicon-32x32.png',
-'./LogoPareja.png',
-// NUEVO asset
-'./IconoJuegoPreguntas.png',
-'./IconoEspejo.png',
-'./IconoEquipo.png',
-'./IconoFuego.png',
-'./IconoEspejoInner.png',
-'./IconoEquipoInner.png',
-'./IconoFuegoInner.png',
-'./IconoBack.png',
-'https://fonts.googleapis.com/css2?family=Shrikhand&display=swap'
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './questions_pareja.json',
+  
+  // Lista de todos los assets gráficos
+  './apple-touch-icon.png',
+  './favicon.ico',
+  './android-chrome-192x192.png',
+  './android-chrome-512x512.png', // <-- CORRECCIÓN AQUÍ
+  './favicon-16x16.png',
+  './favicon-32x32.png',
+  './LogoPareja.png',
+  './IconoJuegoPreguntas.png',
+  './IconoEspejo.png',
+  './IconoEquipo.png',
+  './IconoFuego.png',
+  './IconoEspejoInner.png',
+  './IconoEquipoInner.png',
+  './IconoFuegoInner.png',
+  './IconoBack.png',
+  
+  'https://fonts.googleapis.com/css2?family=Shrikhand&display=swap'
 ];
+
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
 self.addEventListener('activate', e => {
-const cacheWhitelist = [CACHE_NAME];
-e.waitUntil(caches.keys().then(names => Promise.all(names.map(name => { if (cacheWhitelist.indexOf(name) === -1) { return caches.delete(name); } }))));
+  const cacheWhitelist = [CACHE_NAME];
+  e.waitUntil(caches.keys().then(names => Promise.all(names.map(name => { if (cacheWhitelist.indexOf(name) === -1) { return caches.delete(name); } }))));
 });
